4b93b261c4add763f31f5b66e3e7be054796e313
otobit-shreya <arshreya@otobit.in>
2022-09-27 18:12:45 +0530

    mui install

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -1,37 +1,31 @@
 import React, { useState } from "react";
-import { Grid, Box, Checkbox} from "@mui/material";
+import { Grid, Box, Checkbox } from "@mui/material";
 import classes from "./UserForm.module.css";
 
-
-const UserForm: React.FC= () => {
+const UserForm: React.FC = () => {
   const [userName, setUserName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState<string>("");
   const [mobileNo, setMobileNo] = useState<string | number>("");
   const [role, setRole] = useState("");
   const [lockUser, setLockUser] = useState(false);
- 
 
- 
-
-  const checkChangeHandler:any= () => {
+  const checkChangeHandler: any = () => {
     setLockUser(!lockUser);
   };
 
- 
-
-  const formSubmitHandler = (e:React.FormEvent) =>{
+  const formSubmitHandler = (e: React.FormEvent) => {
     e.preventDefault();
-    const info = {userName, email, password, mobileNo, role, lockUser};
-    console.log(info); 
-    
+    const info = { userName, email, password, mobileNo, role, lockUser };
+    console.log(info);
+
     setUserName("");
     setEmail("");
     setPassword("");
     setMobileNo("");
     setRole("");
     setLockUser(checkChangeHandler);
-  }
+  };
 
   return (
     <form onSubmit={formSubmitHandler}>
@@ -41,7 +35,6 @@ const UserForm: React.FC= () => {
           marginTop: "2rem",
           width: 500,
           height: 350,
-          
 
           //    backgroundColor:"lightgray"
         }}
@@ -53,11 +46,12 @@ const UserForm: React.FC= () => {
             </label>
           </Grid>
           <Grid item lg={6} xs={5} sm={6}>
-            <input 
-            type="text" 
-            id="username" 
-            onChange={(e)=>setUserName(e.target.value)}
-             value={userName}/>
+            <input
+              type="text"
+              id="username"
+              onChange={(e) => setUserName(e.target.value)}
+              value={userName}
+            />
           </Grid>
           <Grid item lg={6} xs={10} sm={12}>
             <label htmlFor="email">
@@ -65,11 +59,12 @@ const UserForm: React.FC= () => {
             </label>
           </Grid>
           <Grid item lg={6} xs={5} sm={6}>
-            <input 
-            type="email" 
-            id="email" 
-            onChange={(e)=>setEmail(e.target.value)} 
-            value={email} />
+            <input
+              type="email"
+              id="email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
           </Grid>
           <Grid item lg={6} xs={10} sm={12}>
             <label htmlFor="password">
@@ -80,7 +75,7 @@ const UserForm: React.FC= () => {
             <input
               type="password"
               id="password"
-              onChange={(e)=>setPassword(e.target.value)}
+              onChange={(e) => setPassword(e.target.value)}
               value={password}
             />
           </Grid>
@@ -90,11 +85,12 @@ const UserForm: React.FC= () => {
             </label>
           </Grid>
           <Grid item lg={6} xs={5} sm={6}>
-            <input type="number"
-             id="mobile" 
-             onChange={(e)=>setMobileNo(e.target.value)}
-             value={mobileNo}
-             />
+            <input
+              type="number"
+              id="mobile"
+              onChange={(e) => setMobileNo(e.target.value)}
+              value={mobileNo}
+            />
           </Grid>
           <Grid item lg={6} xs={10} sm={12}>
             <label>
@@ -102,8 +98,8 @@ const UserForm: React.FC= () => {
             </label>
           </Grid>
           <Grid item lg={6} xs={5} sm={6}>
-            <select onChange={(e)=>setRole(e.target.value)} value={role}>
-              <option >Select</option>
+            <select onChange={(e) => setRole(e.target.value)} value={role}>
+              <option>Select</option>
               <option>Floor Manager</option>
               <option>Option2</option>
               <option>Option3</option>
@@ -117,17 +113,16 @@ const UserForm: React.FC= () => {
           </Grid>
           <Grid item lg={6} xs={12} sm={12} className={classes.checkbox}>
             <Checkbox
-            
-          
-            size="small" 
-            checked={lockUser} 
-            onChange={checkChangeHandler}
-          
+              size="small"
+              checked={lockUser}
+              onChange={checkChangeHandler}
             />
           </Grid>
           <Grid item lg={6} xs={12} sm={12}></Grid>
           <Grid item lg={6} xs={12} sm={12}>
-            <button className={classes.actions} type="submit">Submit</button>
+            <button className={classes.actions} type="submit">
+              Submit
+            </button>
           </Grid>
         </Grid>
       </Box>
